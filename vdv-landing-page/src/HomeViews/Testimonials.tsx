7791730d6ba5e2e82d34b0f30b16f16d6c642a03
Georgina Zeron <93169706+930r91na@users.noreply.github.com>
2024-04-19 12:31:22 -0600

    Revert "Se agregó el carrusel que ni gira alch, pero se ordenaron los productos de forma horizontal"
    This reverts commit 91ffd4834dc7f5688f24d8b4bb3292b12ef88e3f.

--- vdv-landing-page/src/HomeViews/Testimonials.tsx
+++ vdv-landing-page/src/HomeViews/Testimonials.tsx
@@ -33,7 +33,7 @@ const demoTestimonials = [
 ];
 
 export default function EmblaCarouselTestimonials() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });//, [Autoplay()]);
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false }, [Autoplay()]);
 
   useEffect(() => {
     if (emblaApi) {
