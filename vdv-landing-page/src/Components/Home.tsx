91ffd4834dc7f5688f24d8b4bb3292b12ef88e3f
ximenavm7 <ximena.vazquezmelladofs@udlap.mx>
2024-04-19 11:48:07 -0600

    Se agregó el carrusel que ni gira alch, pero se ordenaron los productos de forma horizontal

--- vdv-landing-page/src/Components/Home.tsx
+++ vdv-landing-page/src/Components/Home.tsx
@@ -4,43 +4,9 @@ import EmblaCarouselTestimonials from "../HomeViews/Testimonials";
 import OurProcess from "../HomeViews/OurProcess";
 import OurServices from "../HomeViews/OurServices";
 import { FloatingWhatsApp } from "react-floating-whatsapp";
-import OurProducts from "../HomeViews/OurProducts";
+import { EmblaCarouselProducts } from "../HomeViews/ProductsCarousel";
+import logo from "../assets/VDV.png";
 
-enum ProductType {
-  water,
-  plastic,
-}
-
-/*const products = [
-  {
-    name: "Botella de 100 ml",
-    description: "Botella con etiqueta personalizada",
-    image: "/../assets/botella.png",
-    price: 10.0,
-    type: ProductType.plastic,
-  },
-  {
-    name: "Botella de  500 ml",
-    description: "Botella con etiqueta personalizada",
-    image: "/../assets/bottle.png",
-    price: 15.0,
-    type: ProductType.plastic,
-  },
-  {
-    name: "Garrafon de  20 lts",
-    description: "Compra de garrafon de agua purificada",
-    image: "/../assets/garrafon.png",
-    price: 50.0,
-    type: ProductType.plastic,
-  },
-  {
-    name: "Garrafon de  20 lts",
-    description: "Llenado de garrafon de agua purificada",
-    image: "/../assets/garrafon.png",
-    price: 15,
-    type: ProductType.water,
-  },
-];*/
 
 export default function Home() {
   return (
@@ -48,7 +14,7 @@ export default function Home() {
       <HeroContent />
       <OurServices />
       <EmblaCarouselTestimonials />
-      <OurProducts />
+      <EmblaCarouselProducts />
       <OurTeam />
       <OurProcess />
 
@@ -56,7 +22,7 @@ export default function Home() {
         //TODO: Replace this with correct information
         phoneNumber="2215304660"
         accountName="Valle del Volcan"
-        avatar="../../public/VDV.png"
+        avatar={logo}
         chatMessage="¡Hola! ¿En qué podemos ayudarte?"
         darkMode={false}
       />
