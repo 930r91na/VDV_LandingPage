17903e7fc495356e7e85949cdd977d19f11d8919
ximenavm7 <ximena.vazquezmelladofs@udlap.mx>
2024-04-19 23:21:01 -0600

    Reorganized Home's products to be horizontal.

--- vdv-landing-page/src/HomeViews/OurProducts.tsx
+++ vdv-landing-page/src/HomeViews/OurProducts.tsx
@@ -1,67 +1,74 @@
-import ProductCard from "../Utils/ProductsCard";
+import React, { useEffect } from 'react';
+import useEmblaCarousel from 'embla-carousel-react';
 import botella from "../assets/b1.jpg";
 import garrafon from "../assets/garrafon.png";
 import botella2 from "../assets/b2.jpg";
 import garrafon2 from "../assets/garrafon2.png";
-import ProductsCarousel from "../Utils/ProductsCarousel";
-enum ProductType {
-  water,
-  plastic,
-}
-
-const products = [
-  {
-    name: "Botella de 100 ml",
-    description: "Botella con etiqueta personalizada",
-    image: botella,
-    price: 10.0,
-    type: ProductType.plastic,
-  },
-  {
-    name: "Botella de  500 ml",
-    description: "Botella con etiqueta personalizada",
-    image: botella2,
-    price: 15.0,
-    type: ProductType.plastic,
-  },
-  {
-    name: "Garrafon de  20 lts",
-    description: "Compra de garrafon de agua purificada",
-    image: garrafon,
-    price: 50.0,
-    type: ProductType.plastic,
-  },
-  {
-    name: "Garrafon de  20 lts",
-    description: "Llenado de garrafon de agua purificada",
-    image: garrafon2,
-    price: 15,
-    type: ProductType.water,
-  },
-];
 
 const OurProducts = () => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true })
+
+  useEffect(() => {
+    if (emblaApi) {
+      console.log(emblaApi.slideNodes()) // Access API
+    }
+  }, [emblaApi])
+
   return (
-    <section className="pt-20">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+    <section>
+    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="font-bold tracking-tight text-gray-900 sm:text-4xl">
           <h1>Productos</h1>
         </div>
       </div>
-      <div className="mx-auto max-w-7xl px-6 lg:px-8 grid grid-cols-1 md:grid-cols-2 gap-8 items-start">
-        
-        <div>
-          {products.map((product) => (
-            <ProductCard
-              name={product.name}
-              description={product.description}
-              imageUrl={product.image}
-              price={product.price}
-            />
-          ))}
+    <div className="flex flex-wrap">
+        <div className="product-container">
+            <div className="subtitulo centrar-contenido">
+                <h1>Botella de 100 ml</h1>
+                <img
+                className="h-32 w-32 md:h-48 md:w-48 rounded object-cover" 
+                src={botella} />
+                <p className="text-lg font-semibold leading-6 text-gray-800">
+                    Precio: ${15}
+                </p>
+            </div>
         </div>
-      </div>
-    </section>
+        <div className="product-container">
+            <div className="subtitulo centrar-contenido">
+                <h1>Botella de 500 ml</h1>
+                <img 
+                className="h-32 w-32 md:h-48 md:w-48 rounded object-cover"
+                src={botella2} />
+                <p className="text-lg font-semibold leading-6 text-gray-800">
+                    Precio: ${20}
+                </p>
+            </div>
+        </div>
+        <div className="product-container">
+            <div className="subtitulo centrar-contenido">
+                <h1>Garrafón de 20 lts</h1>
+                <img
+                className="h-32 w-32 md:h-48 md:w-48 rounded object-cover"
+                src={garrafon}/>
+                <p className="text-lg font-semibold leading-6 text-gray-800">
+                    Precio: ${50}
+                </p>
+            </div>
+        </div>
+        <div className="product-container">
+            <div className="subtitulo centrar-contenido">
+                <h1>Llenado de garrafón de agua pruficiada</h1>
+                <img
+                className="h-32 w-32 md:h-48 md:w-48 rounded object-cover"
+                src={garrafon2} />
+                <p className="text-lg font-semibold leading-6 text-gray-800">
+                    Precio: ${15}
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
   );
-};
+}
+
 export default OurProducts;
